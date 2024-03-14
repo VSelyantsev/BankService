b1681ad5d17b9f2c363be3ae87f1a2af64dd1ed8
VSelyantsev <vselyantsev@mail.ru>
2024-03-14 17:00:24 +0300

    modified SecurityConfig and UserController

--- BankServiceApplication/src/main/java/ru/itis/kpfu/selyantsev/configuration/SecurityConfiguration.java
+++ BankServiceApplication/src/main/java/ru/itis/kpfu/selyantsev/configuration/SecurityConfiguration.java
@@ -3,6 +3,7 @@ package ru.itis.kpfu.selyantsev.configuration;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -13,6 +14,7 @@ import ru.itis.kpfu.selyantsev.filter.JwtFilter;
 
 @Configuration
 @EnableWebSecurity(debug = true)
+@EnableMethodSecurity
 @RequiredArgsConstructor
 public class SecurityConfiguration {
 
--- BankServiceApplication/src/main/java/ru/itis/kpfu/selyantsev/controller/UserController.java
+++ BankServiceApplication/src/main/java/ru/itis/kpfu/selyantsev/controller/UserController.java
@@ -1,7 +1,7 @@
 package ru.itis.kpfu.selyantsev.controller;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.RestController;
 import ru.itis.kpfu.selyantsev.api.UserApi;
 import ru.itis.kpfu.selyantsev.dto.request.*;
@@ -24,32 +24,37 @@ public class UserController implements UserApi {
     }
 
     @Override
+    @PreAuthorize("isAuthenticated()")
     public UserResponse addPhoneNumber(PhoneNumberRequest phoneNumber) {
         return userService.addPhoneNumber(phoneNumber);
     }
 
     @Override
+    @PreAuthorize("isAuthenticated()")
     public UserResponse editPhoneNumber(EditPhoneNumberRequest editPhoneNumberRequest) {
         return userService.editPhoneNumber(editPhoneNumberRequest);
     }
 
     @Override
+    @PreAuthorize("isAuthenticated()")
     public UserResponse addEmail(EmailAddressRequest email) {
         return userService.addEmail(email);
     }
 
     @Override
+    @PreAuthorize("isAuthenticated()")
     public UserResponse editEmail(EditEmailRequest editEmailRequest) {
         return userService.editEmail(editEmailRequest);
     }
 
     @Override
-    @ResponseBody
+    @PreAuthorize("isAuthenticated()")
     public List<PhoneNumberResponse> deletePhoneNumber(PhoneNumberRequest phoneNumberRequest) {
         return userService.deletePhoneNumber(phoneNumberRequest);
     }
 
     @Override
+    @PreAuthorize("isAuthenticated()")
     public List<EmailAddressResponse> deleteEmail(EmailAddressRequest emailAddressRequest) {
         return userService.deleteEmail(emailAddressRequest);
     }
